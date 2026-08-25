00b0691ccb631733d5fea5e17620341c22d53a47
Mateusz Stobiecki <m100biecki@gmail.com>
2026-08-25 16:38:39 +0200

    complete navigation in css/js on small devices

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,13 +1,16 @@
 const displayNavigation = () => {
     const navigationWrapper = document.querySelector('.header-navigation-wrapper'); 
-    const navigationButton = document.querySelector('.header-hamburger-button'); 
+    const navigationButton = document.querySelector('.header-navigation-hamburger'); 
     const navigationLinks = document.querySelectorAll('.header-navigation-link'); 
+    const navigationCloseButton = document.querySelector('.header-navigation-close-button');
 
     const handleNavigation = () => {
         navigationWrapper.classList.toggle('header-navigation-open');
     }
-
+  
     navigationButton.addEventListener('click', handleNavigation); 
+    navigationCloseButton.addEventListener('click', handleNavigation);
+
     navigationLinks.forEach((link) => link.addEventListener('click', handleNavigation)); 
 };
 
