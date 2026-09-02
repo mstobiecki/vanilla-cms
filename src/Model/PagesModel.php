aa252a3849d5fc4bd0b710f77fc299265aca3f69
Mateusz Stobiecki <m100biecki@gmail.com>
2026-09-02 18:41:43 +0200

    added PageModel class

---
+++ src/Model/PagesModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+class PagesModel
+{
+    public int $id;
+    public string $title;
+    public string $slug;
+    public string $image;
+    public string $content;
+}
